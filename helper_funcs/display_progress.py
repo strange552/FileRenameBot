94a6cf4d4ffad8f0dfa4c8c0f2c34b387187548a
Adrew <64140254+strange552@users.noreply.github.com>
2020-10-01 11:11:29 +0530

    Update display_progress.py

--- helper_funcs/display_progress.py
+++ helper_funcs/display_progress.py
@@ -42,12 +42,12 @@ async def progress_for_pyrogram(
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = "[{0}{1}] \n <b>📊Percentage:</b> {2}%\n".format(
-            ''.join(["■" for i in range(math.floor(percentage / 5))]),
-            ''.join(["□" for i in range(20 - math.floor(percentage / 5))]),
+        progress = "[{0}{1}] \n <b>Percentage:</b> {2}%\n".format(
+            ''.join(["🟥" for i in range(math.floor(percentage / 5))]),
+            ''.join(["🟨" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
 
-        tmp = progress + "<b>✅Completed:</b>{0} \n<b>📁Total Size:</b> {1}\n<b>🚀Speed:</b> {2}/s\n<b>⌚️ETA:</b> {3}\n".format(
+        tmp = progress + "<b>Completed ✅:</b>{0} \n<b>Total Size 📁:</b> {1}\n<b>Speed 🚀:</b> {2}/s\n<b>ETA ⏳:</b> {3}\n".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
