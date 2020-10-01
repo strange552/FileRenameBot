5c66a989c24c18ca86884dfc5ab2b7f1569e338a
Adrew <64140254+strange552@users.noreply.github.com>
2020-10-01 11:17:36 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -70,12 +70,12 @@ async def start(bot, update):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('Support Channel', url='https://t.me/Mai_bOTs'),
-                    InlineKeyboardButton('Feedback', url='https://t.me/No_OnE_Kn0wS_Me')
+                    InlineKeyboardButton('Movie Channel 🧡', url='https://t.me/XstreamMovies'),
+                    InlineKeyboardButton('Feedback ?', url='https://t.me/AdrewJons')
                 ],
                 [
-                    InlineKeyboardButton('Other Bots', url='https://t.me/Mai_bOTs/17'),
-                    InlineKeyboardButton('Source', url='https://github.com/No-OnE-Kn0wS-Me/FileRenameBot')
+                    InlineKeyboardButton('Other Channel 🧡', url='https://t.me/File2U'),
+                    InlineKeyboardButton('Movie Group 🧡', url='https://t.me/orderOFmovies')
                 ]
             ]
         ),
